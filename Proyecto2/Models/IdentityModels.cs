c48cc43efbe13f543511904d4092f42f68393d5f
JMONTERROSO13 <jorgeantmonasp13@hotmail.com>
2015-04-19 08:37:46 -0600

    INITIAL COMMIT
    USERS WITH ROLES AND PEOPLE

---
+++ Proyecto2/Models/IdentityModels.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Proyecto2.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        [StringLength(13)]
+        [RegularExpression(@"^[0-9]+$", ErrorMessage = "Ingrese un DPI correcto")]
+        public string Dpi { get; set; }
+
+        [Required]
+        [StringLength(60, MinimumLength = 3)]
+        public string Nombre { get; set; }
+
+        [Required]
+        [StringLength(60, MinimumLength = 3)]
+        public string Apellido { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(200, MinimumLength = 20)]
+        public string Direccion { get; set; }
+         
+        [Required]
+        [StringLength(11, MinimumLength = 8)]
+        [RegularExpression(@"^[0-9]+$", ErrorMessage = "Ingrese un numero telefonico correcto")]
+        public string Telefono { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 10)]
+        public string Referencia1 { get; set; }
+
+        [Required]
+        [StringLength(10, MinimumLength = 8)]
+        [RegularExpression(@"^[0-9]+$", ErrorMessage = "Ingrese un numero telefonico correcto")]
+        public string TelefonoReferencia1 { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 10)]
+        public string Referencia2 { get; set; }
+
+        [Required]
+        [StringLength(12, MinimumLength = 8)]
+        [RegularExpression(@"^[0-9]+$", ErrorMessage = "Ingrese un numero telefonico correcto")]
+        public string TelefonoReferencia2 { get; set; }
+
+
+    }
+
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection")
+        {
+
+        }
+    }
+
+
+    public class IdentityManager
+    {
+        public bool RoleExists(string name)
+        {
+            var rm = new RoleManager<IdentityRole>(
+                new RoleStore<IdentityRole>(new ApplicationDbContext()));
+            return rm.RoleExists(name);
+        }
+
+
+        public bool CreateRole(string name)
+        {
+            var rm = new RoleManager<IdentityRole>(
+                new RoleStore<IdentityRole>(new ApplicationDbContext()));
+            var idResult = rm.Create(new IdentityRole(name));
+            return idResult.Succeeded;
+        }
+
+
+        public bool CreateUser(ApplicationUser user, string password)
+        {
+            var um = new UserManager<ApplicationUser>(
+                new UserStore<ApplicationUser>(new ApplicationDbContext()));
+            var idResult = um.Create(user, password);
+            return idResult.Succeeded;
+        }
+
+
+        public bool AddUserToRole(string userId, string roleName)
+        {
+            var um = new UserManager<ApplicationUser>(
+                new UserStore<ApplicationUser>(new ApplicationDbContext()));
+            var idResult = um.AddToRole(userId, roleName);
+            return idResult.Succeeded;
+        }
+
+
+        public void ClearUserRoles(string userId)
+        {
+            var um = new UserManager<ApplicationUser>(
+                new UserStore<ApplicationUser>(new ApplicationDbContext()));
+            var user = um.FindById(userId);
+            var currentRoles = new List<IdentityUserRole>();
+            currentRoles.AddRange(user.Roles);
+            foreach (var role in currentRoles)
+            {
+                um.RemoveFromRole(userId, role.Role.Name);
+            }
+        }
+    }
+}
